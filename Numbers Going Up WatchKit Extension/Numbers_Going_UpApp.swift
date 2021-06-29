72f5359c38e31a64052c9f3996396e50a3bfd4c8
buresdv <buresdv@gmail.com>
2021-06-29 14:31:44 +0200

    Initial Commit

---
+++ Numbers Going Up WatchKit Extension/Numbers_Going_UpApp.swift
@@ -0,0 +1,19 @@
+//
+//  Numbers_Going_UpApp.swift
+//  Numbers Going Up WatchKit Extension
+//
+//  Created by David Bureš on 29.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct Numbers_Going_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
